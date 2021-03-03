7e17395ce31170285198879d6ecb877ab1334dc3
zdegraaf <69164506+zdegraaf@users.noreply.github.com>
2021-03-03 12:45:50 -0500

    Update install-proxy.sh

--- install-proxy.sh
+++ install-proxy.sh
@@ -11,36 +11,37 @@ fi
 	echo "Enter Zabbix server Hostname or IP"
 	read -a zbx_srv -p "Zabbix Server: "; echo
 #Install docker and docker compose 
-	apt install docker docker-compose jq -y
+	echo "Installing required packages"
+	apt install docker docker-compose jq zabbix-agent -y 1> /dev/null
 #Find current user
+	echo "Making user changes"
 	user=$(who | awk '{print $1}' | uniq)
 #Add current user to docker group
-	usermod -a -G docker $user
+	usermod -a -G docker $user 1> /dev/null
 #Create directories for docker
-	mkdir /docker 
-	mkdir /docker/proxy
-	mkdir /docker/proxy/externalscripts
-	mkdir /docker/proxy/enc
-	mkdir /docker/compose
-	mkdir /docker/compose/proxy
+	echo "Making required directories"
+	mkdir /docker 1> /dev/null
+	mkdir /docker/proxy 1> /dev/null
+	mkdir /docker/proxy/externalscripts 1> /dev/null
+	mkdir /docker/proxy/enc 1> /dev/null
+	mkdir /docker/compose 1> /dev/null
+	mkdir /docker/compose/proxy 1> /dev/null
 #Change permisions to current user and docker group
-	chown -R $user:docker /docker
+	chown -R $user:docker /docker 1> /dev/null
 #Generate TLS key for encrypted connection
-	tls_key=$(openssl rand -hex 32)
-	echo $tls_key > /docker/proxy/enc/tls.psk
+	tls_key=$(openssl rand -hex 32) 
+	echo $tls_key > /docker/proxy/enc/tls.psk 
 #Replace variables in docker compose file
 	cat TEMPLATE-docker-compose.yml | sed "s/PSK 111/PSK $tls_id/g" | sed "s/example-hostname/$hostname/g" | sed "s/example.host.com/$zbx_srv/g" > /docker/compose/proxy/docker-compose.yml
 #Start the docker container
-docker-compose -f /docker/compose/proxy/docker-compose.yml up -d
-#Install Zabbix agent
-	apt install zabbix-agent -y
+docker-compose -f /docker/compose/proxy/docker-compose.yml up -d 1> /dev/null
 #Edit Zabbix config file
 	cat /etc/zabbix/zabbix_agentd.conf | sed "s/ServerActive=127.0.0.1/ServerActive=$zbx_srv/g" | sed "s/# Hostname=/Hostname=$hostname/g" | sed 's/# TLSConnect=unencrypted/TLSConnect=psk/g' | sed 's/# TLSAccept=unencrypted/TLSAccept=psk/g' | sed "s/# TLSPSKIdentity=/TLSPSKIdentity=PSK $tls_id/g" | sed 's/# TLSPSKFile=/TLSPSKFile=\/docker\/proxy\/enc\/tls.psk/g' > /etc/zabbix/zabbix_agentd.conf.tmp
-	mv /etc/zabbix/zabbix_agentd.conf.tmp /etc/zabbix/zabbix_agentd.conf
+	mv /etc/zabbix/zabbix_agentd.conf.tmp /etc/zabbix/zabbix_agentd.conf 1> /dev/null
 #Move Agent config file
-	cp docker-simple.conf /etc/zabbix/zabbix_agentd.conf.d/docker-simple.conf
+	cp docker-simple.conf /etc/zabbix/zabbix_agentd.conf.d/docker-simple.conf 1> /dev/null
 #Restart zabbix service
-	service zabbix-agent restart
+	service zabbix-agent restart 1> /dev/null
 #Print Output for Zabbix configuration
 	echo "Use these variables to add the Proxy/Host to Zabbix"
 	echo "Proxy name: "$hostname
