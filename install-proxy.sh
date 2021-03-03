2b0d12f2dbc287fe7444f8d39d7eedecc623d9e2
zdegraaf <69164506+zdegraaf@users.noreply.github.com>
2021-03-03 09:39:35 -0500

    Update install-proxy.sh

--- install-proxy.sh
+++ install-proxy.sh
@@ -30,7 +30,7 @@ docker-compose -f /docker/compose/proxy/docker-compose.yml up -d
 #Install Zabbix agent
 	apt install zabbix-agent -y
 #Edit Zabbix config file
-	cat /etc/zabbix/zabbix_agentd.conf | sed "s/ServerActive=127.0.0.1/ServerActive=zabbix.iserv.net/g" | sed "s/# Hostname=/Hostname=$hostname/g" | sed 's/# TLSConnect=unencrypted/TLSConnect=psk/g' | sed "s/# TLSPSKIdentity=/TLSPSKIdentity=PSK $tls_id/g" | sed 's/# TLSPSKFile=/TLSPSKFile=\/docker\/proxy\/enc\/tls.psk/g' > /etc/zabbix/zabbix_agentd.conf
+	cat /etc/zabbix/zabbix_agentd.conf | sed "s/ServerActive=127.0.0.1/ServerActive=$zbx_srv/g" | sed "s/# Hostname=/Hostname=$hostname/g" | sed 's/# TLSConnect=unencrypted/TLSConnect=psk/g' | sed "s/# TLSPSKIdentity=/TLSPSKIdentity=PSK $tls_id/g" | sed 's/# TLSPSKFile=/TLSPSKFile=\/docker\/proxy\/enc\/tls.psk/g' > /etc/zabbix/zabbix_agentd.conf
 #Move Agent config file
 	mv docker-simple.conf /etc/zabbix/zabbix_agentd.conf.d/docker-simple.conf
 #Restart zabbix service
