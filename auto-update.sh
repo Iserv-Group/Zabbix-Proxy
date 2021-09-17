a0ec718cf4054d94e5621bb20c6e9508c79fc22d
zdegraaf <69164506+zdegraaf@users.noreply.github.com>
2021-09-17 15:37:32 -0400

    Update auto-update.sh

--- auto-update.sh
+++ auto-update.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #Script for autmatically keeping Zabbix Proxies up to date
 
-#Copy current version of GIthub repo and overwrite existing
+#Copy current version of Github repo
 git clone https://github.com/Iserv-Group/Zabbix-Proxy.git ~/temp
-mv ~/temp/ ~/Zabbix-Proxy/
+cp -fr ~/temp/* -t ~/Zabbix-Proxy
+rm -fr ~/temp/
 #Copy all scripts to Zabbix docker folder
 cp -r ~/Zabbix-Proxy/externalscripts/ /docker/proxy/
 #Pull Current version of Proxy docker container and rebuild the container 
 docker-compose -f /docker/compose/proxy/docker-compose.yml pull
 docker-compose -f /docker/compose/proxy/docker-compose.yml up -d
+#Reset new version of this update script to executable
+
 
