6842b6d7f771cd222533d09b34f1b79c5a951fc1
zdegraaf <69164506+zdegraaf@users.noreply.github.com>
2023-03-28 15:15:57 -0400

    Update auto-update.sh
    Add code to set the version of the proxy

--- auto-update.sh
+++ auto-update.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+tag="latest" #Set tag that proxies will use
 #Script for autmatically keeping Zabbix Proxies up to date
-
 #Copy current version of Github repo
 git clone https://github.com/Iserv-Group/Zabbix-Proxy.git ~/temp
 cp -fr ~/temp/* -t ~/Zabbix-Proxy
 rm -fr ~/temp/
 #Copy all scripts to Zabbix docker folder
 cp -r ~/Zabbix-Proxy/externalscripts/ /docker/proxy/
+#Set Proxy Tag
+image='    image: zabbix\/zabbix-proxy-sqlite3:'$tag
+sed -i "s/.*zabbix-proxy-sqlite3.*/$image/" docker-compose.yml
 #Pull Current version of Proxy docker container and rebuild the container 
 docker-compose -f /docker/compose/proxy/docker-compose.yml pull
 docker-compose -f /docker/compose/proxy/docker-compose.yml up -d
 #Reset new version of this update script to executable
-
+chmod +X ~/Zabbix-Proxy/auto-update.sh
 
